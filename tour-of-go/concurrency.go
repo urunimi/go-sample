c73f320611e484b07f8500462048a610856c5ad4
Ben Pyungwoo Yoo <hovan@hovans.com>
2016-12-16 12:30:25 +0900

    Mutex

--- tour-of-go/concurrency.go
+++ tour-of-go/concurrency.go
@@ -2,17 +2,125 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	defaultSelection()
+	//channels()
 
-	selectFunc()
+	//rangeAndClose()
 
-	rangeAndClose()
+	//selectFunc()
 
-	channels()
+	//defaultSelection()
+
+	//binaryTrees()
+
+	mutex()
+}
+
+/*
+We've seen how channels are great for communication among goroutines.
+
+But what if we don't need communication? What if we just want to make sure only one goroutine can access a variable at a time to avoid conflicts?
+
+This concept is called mutual exclusion, and the conventional name for the data structure that provides it is mutex.
+
+Go's standard library provides mutual exclusion with sync.Mutex and its two methods:
+
+Lock
+Unlock
+We can define a block of code to be executed in mutual exclusion by surrounding it with a call to Lock and Unlock as shown on the Inc method.
+
+We can also use defer to ensure the mutex will be unlocked as in the Value method.
+*/
+func mutex() {
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.Inc("somekey")
+	}
+	time.Sleep(time.Nanosecond * 100)
+	fmt.Println(c.Value("somekey"))
+}
+
+type SafeCounter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+func (c *SafeCounter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
+/*
+There can be many different binary trees with the same sequence of values stored at the leaves. For example, here are two binary trees storing the sequence 1, 1, 2, 3, 5, 8, 13.
+*/
+func binaryTrees() {
+	cSize := 7
+	c := make(chan int, cSize)
+
+	head := &Tree{
+		Left: &Tree{
+			Left: &Tree{
+				Value: 1,
+			},
+			Value: 1,
+			Right: &Tree{
+				Value: 2,
+			},
+		},
+		Value: 3,
+		Right: &Tree{
+			Left: &Tree{
+				Value: 5,
+			},
+			Value: 8,
+			Right: &Tree{
+				Value: 13,
+			},
+		},
+	}
+
+	go Walk(head, c)
+
+	for i := 0; i < cSize; i++ {
+		fmt.Println(<-c)
+	}
+}
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *Tree, ch chan int) {
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	time.Sleep(time.Millisecond * 100)
+
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *Tree) bool {
+	return t1.Value == t2.Value
+}
+
+type Tree struct {
+	Left  *Tree
+	Value int
+	Right *Tree
 }
 
 /*
